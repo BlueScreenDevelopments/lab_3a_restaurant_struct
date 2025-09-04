691a6ad1682df3a73e2afcf57516760eeb8fe0b8
Brandon J <bsjulao@gmail.com>
2025-09-03 19:45:06 -0700

    Commit

--- lab_3a_restaurant_struct/lab3arestaurantstruct.cpp
+++ lab_3a_restaurant_struct/lab3arestaurantstruct.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 
-/*/dfgdfgdfg*/
+/*
+Brandon Julao
+COMSC-210-5470
+09.03.25
+Restaurant Struct
+
+*/
 
 int main(){
-    cout << "IT'S HAPPENING...\n"
+    cout << "IT'S HAPPENING...\n";
 
-    return 0;
-}
+
+    
+};
